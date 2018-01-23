ac6f327d4cb75eb1715d1f9eb150f0bfb50197e5
Nick Lee <nickoless.lee@gmail.com>
2018-01-23 08:10:22 +0000

    Added formatting, comments

--- download_avatars.js
+++ download_avatars.js
@@ -4,9 +4,11 @@ var fs = require('fs');
 
 var filename = 0;
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
+// generates URL based on parameters
 function getRepoContributors(repoOwner, repoName, cb) {
   if (repoOwner === undefined | repoName === undefined) {
     console.log('\n[-- Error: please enter values for both <repoOwner> and <repoName>. --]');
@@ -19,29 +21,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
       }
     };
 
+    // callback
     request(options, function(err, res, body) {
       cb(err, body);
+
     });
   }
 }
 
+// downloads image to 'avatar' folder
 function downloadImageByURL(url, filePath) {
   request(url)
-    .on('error', function(err){
+    .on('error', function(err) {
       throw err;
     })
-
-    .on('end', function(end){
+    .on('end', function(end) {
       console.log('Download of avatar ' + (filename++) + ' complete.');
     })
-
     .pipe(fs.createWriteStream(filePath + (filename++) + '.jpg'));
 }
 
-getRepoContributors(process.argv[2], process.argv[3], function(err, result){
+
+getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
   console.log("Errors:", err);
   var sort = JSON.parse(result);
-  sort.forEach(function(avatar){
+  sort.forEach(function(avatar) {
     downloadImageByURL(avatar.avatar_url, "avatars/pic");
   });
 });

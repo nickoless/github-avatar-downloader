b3f38f1aa2ae6efe9d1a91e0da6ed06d075f8f0c
Nick Lee <nickoless.lee@gmail.com>
2018-01-23 07:52:15 +0000

    Added process.argv functionality, cleaned code

--- download_avatars.js
+++ download_avatars.js
@@ -1,30 +1,49 @@
 var request = require('request');
 var secrets = require('./secrets').GITHUB_TOKEN;
+var fs = require('fs');
 
+var filename = 0;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': 'token ' + secrets
-    }
-  };
+  if (repoOwner === undefined | repoName === undefined) {
+    console.log('\n[-- Error: please enter values for both <repoOwner> and <repoName>. --]');
+  } else {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': 'token ' + secrets
+      }
+    };
 
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
+  }
+}
 
-  request(options, function(err, res, body) {
-    cb(err, body);
+function downloadImageByURL(url, filePath) {
+  request(url)
+    .on('error', function(err){
+      throw err;
+    })
 
-  });
- }
+    .on('end', function(end){
+      console.log('Download of avatar ' + (filename++) + ' complete.');
+    })
 
-getRepoContributors("jquery", "jquery", function(err, result){
+    .pipe(fs.createWriteStream(filePath + (filename++) + '.jpg'));
+}
+
+getRepoContributors(process.argv[2], process.argv[3], function(err, result){
   console.log("Errors:", err);
   var sort = JSON.parse(result);
   sort.forEach(function(avatar){
-  console.log(avatar.avatar_url);
+    downloadImageByURL(avatar.avatar_url, "avatars/pic");
   });
 });
+
 

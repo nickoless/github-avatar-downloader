84158666f0ae9cce05c650375b36931d05558f68
Nick Lee <nickoless.lee@gmail.com>
2018-01-23 04:19:16 +0000

    Added functionality to print each avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,30 @@
 var request = require('request');
 var secrets = require('./secrets').GITHUB_TOKEN;
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: "https://api.github.com/repos" + repoOwner + "/" + repoName + "/contributors",
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': ''
+      'Authorization': 'token ' + secrets
     }
   };
 
-  request(options, function(err, res, body){
+
+  request(options, function(err, res, body) {
     cb(err, body);
+
   });
  }
 
 getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Errors:", err);
-  console.log("Result:", result);
+  var sort = JSON.parse(result);
+  sort.forEach(function(avatar){
+  console.log(avatar.avatar_url);
+  });
 });
+

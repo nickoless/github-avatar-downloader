6039d7d7192cd74d8acad3495a1e69609c9c6a63
Nick Lee <nickoless.lee@gmail.com>
2018-01-23 02:33:35 +0000

    Added headers to function

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,21 @@
 var request = require('request');
+var secrets = require('./secrets').GITHUB_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(reoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': ''
+    }
+  };
 
-}
+  request(options, function(err, res, body){
+    cb(err, body);
+  });
+ }
 
 getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Errors:", err);
